3175d646cdc13778811261e4a011bfb5a3a5db8b
Cuong Pham <cpham006@vt.edu>
2022-03-31 18:07:03 -0400

    done authentication

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -21,17 +21,5 @@ export class UserService {
   register(user: User) {
     return this.http.post(`http://localhost:3030/user/register`, user);
   }
-  setGoal(calories: number, minutes: number){
-    console.log(calories);
-    console.log(minutes);
-    const values = {
-      calories,
-      minutes
-    }
-    return this.http.post(`http://localhost:3030/user/setgoals`, values);
-  }
 
-  getGoal(res: User){
-    return this.http.get<User>(`http://localhost:3030/user/getgoals/${res}`);
-  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,6 @@ import {first, mergeMap} from 'rxjs/operators';
 
 
 import {NotificationService} from '../_services/notification.service';
-import {PARecord} from '../_models/PARecord';
-import {PArecordService} from '../_services/parecord.service';
 import {UserService} from '../_services/user.service';
 
 
@@ -15,54 +13,19 @@ export class HomeComponent implements OnInit {
 
 
 
-  parecords: PARecord[] = [];
 
 
   constructor(
-    private parecordservice: PArecordService,
 
     private notifService: NotificationService,
   ) {}
 
   ngOnInit() {
-    this.loadAllPArecords();
+
       }
 
 
 
-
-
-  private loadAllPArecords() {
-    console.log('loadAllParecords() ');
-    this.parecordservice.getAll().subscribe(
-         parecords => {
-           this.parecords = parecords;
-         },
-        error => {
-            this.notifService.showNotif(error.toString(), 'warning'); });
-  }
-
-  createPARecord() {
-    this.parecordservice.add().pipe(first()).subscribe(
-      resp => {
-        this.notifService.showNotif(resp, 'response');
-        this.parecords = null;
-        this.loadAllPArecords();
-        console.log("response");
-        }, error => {
-        this.notifService.showNotif(error); });
-  }
-
-  deletePARecord(date) {
-
-
-    // this.userService.deleteActivity(date);
-    this.parecordservice.delete(date).pipe(first()).subscribe( () => {
-      console.log("hit");
-      this.parecords = null;
-      this.loadAllPArecords();
-    });
-  }
 
 }
 
